887d30f15083d5b46c082420d69c2ca5ae150e14
nargysc <eliasnargiz@gmail.com>
2022-09-24 18:38:56 +0300

    Created Contact Form folder

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/footer";
 import Promotions from "./components/promotions";
 import SearchBox from "./components/search";
 import { useEffect } from "react";
-
+import {Contactform} from "./components/contactform";
 function App() {
   useEffect(() => {
     document.title = "Glazier.";
@@ -34,6 +34,7 @@ function App() {
               <Typography variant="h4">Our Collection</Typography>
             </Box>
             <Products />
+            <Contactform/>
             <Footer />
           </UIProvider>
         </Stack>
